ab054c954e7e8f4aa936d7ab49e6086b632c47bf
skillztec <skillztech19@gmail.com>
2023-05-07 17:11:04 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//KINDS OF API CALL TO MAKE
+//GET
+//http://[::1]:4000/products/P001
+//paginate the res data
+// http://[::1]:4000/products?_page=1&_limit=2
+//sorting the res data
+// http://[::1]:4000/products?_sort=price&_order=desc
+//DELETE
+// http://[::1]:4000/products/P006
+//PUT
+// http://[::1]:4000/products/P007
